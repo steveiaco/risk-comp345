26cd8974850d2004d8331cbc504a363d5aadb5c9
Name cannot be found <you@example.com>
2018-10-11 22:28:44 -0400

    Added Driver part 3

--- RISK_01/Dice.h
+++ RISK_01/Dice.h
@@ -4,6 +4,8 @@
 #include <list>
 #include <array>
 #include <iostream>
+#include <cstdlib>
+
 class Dice
 {
 public:
---
+++ RISK_01/Driver_Part_3.h
@@ -0,0 +1,8 @@
+#pragma once
+class Driver_Part_3
+{
+public:
+	Driver_Part_3();
+	~Driver_Part_3();
+};
+
