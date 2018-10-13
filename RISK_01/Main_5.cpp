04565193ef5c8056c128eda712125dacf741f95b
steveiaco <31911579+steveiaco@users.noreply.github.com>
2018-10-12 23:54:13 -0400

    Main_5.cpp updated

--- RISK_01/Main_5.cpp
+++ RISK_01/Main_5.cpp
@@ -1,32 +1,38 @@
 #include "Player.h"
 #include "Deck.h"
-/**
+#include "Map.h"
+
 int main()
 {
+	std::cout << "=======================================\n";
+	std::cout << "				 RISK MAIN 5             \n";
+	std::cout << "=======================================\n";
 
-	std::vector<int> test(5);
+	//Create default map
+	Map* map = new Map();
+	map->display();
 
-	Deck* manualDeck = new Deck();
+	//Create deck from map
+	std::cout << "****SETTING UP DECK****";
+	Deck* manualDeck = new Deck(map);
+	//manualDeck->display();
 
-	Card* card1 = new Card(Card::Infantry);
-	Card* card2 = new Card(Card::Artillery);
-	Card* card3 = new Card(Card::Cavalry);
-	Card* card4 = new Card(Card::Infantry);
+	//Create hand object
+	std::cout << "****SETTING UP HAND****";
+	Hand* hand = new Hand();
+	hand->giveCard(manualDeck->draw());
+	hand->giveCard(manualDeck->draw());
+	hand->giveCard(manualDeck->draw());
 
-	card1->toString();
-	card2->toString();
-	card3->toString();
-	card4->toString();
 
-	manualDeck->addCard(*card1);
-	manualDeck->addCard(*card2);
-	manualDeck->addCard(*card3);
-	manualDeck->addCard(*card4);
+	
 
-	Player player1("Hello");
+		
+
+
+	Player player1("Player 1");
 	for (int i = 0; i < 10; i++) {
 		std::cout << player1.exchange();
 	}
 
 }
-**/
