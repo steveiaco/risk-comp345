4692dc1fca8f0eb1b885362846528b93b01fa66c
bob <alexandre.therrien3@gmail.com>
2018-10-28 14:58:38 -0400

    Added Controller

--- RISK_01/Player.h
+++ RISK_01/Player.h
@@ -23,6 +23,10 @@ private:
 	Hand* playerHand;
 	Dice* dice;
 
+	/*
+	int troops;
+	*/
+
 	
 	//Country* queryOwnedCountry(); //queries the user for a name, attempts to find the country who's name is equal to that string and returns it
 
@@ -37,6 +41,9 @@ public:
 	//Accessors
 	std::string getName() const;
 	void displayHand();
+	/*
+	int getTroops() const;
+	*/
 
 	//Mutators
 	void addCard(Card* card);
@@ -44,6 +51,10 @@ public:
 	void addContinent(Continent* continent);
 	bool removeCountry(Country* country);
 	bool removeContinent(Continent* continent);
+
+	/*
+	int setTroops(int value);
+	*/
 
 	//Utility
 	void reinforce(Country* toReinforce, int numTroops); //count number of countries / 3, count how many continents owned
