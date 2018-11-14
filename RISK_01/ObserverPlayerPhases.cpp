8be2e760f7ff0a21e431309ee51313d840bef4c7
bob <alexandre.therrien3@gmail.com>
2018-11-14 12:55:00 -0500

    Observer completed. About to add dependencies in other files

---
+++ RISK_01/ObserverPlayerPhases.cpp
@@ -0,0 +1,26 @@
+#include "ObserverPlayerPhases.h"
+
+std::string ObserverPlayerPhases::getWholeMessage(){
+    return header+"\n"+description;
+}
+
+//call this method by indicating which phase the player's turn is at (1 - Reinforcement,
+//2 - Attack Phase, 3 - Fortification Phase) and the number attributed to the player
+void ObserverPlayerPhases::setHeaderDescription(int phaseNumber2, int playerNumber2){
+    phaseNumber = phaseNumber2;
+    playerNumber = playerNumber2;
+    setPhaseMessage();
+}
+
+void ObserverPlayerPhases::setPhaseMessage(){
+    switch(phaseNumber){
+        case 1: header = "Player "+std::to_string(playerNumber)+": Reinforcement Phase";
+                description = "Place a number of armies in your countries";break;
+        case 2: header = "Player "+std::to_string(playerNumber)+": Attack Phase";
+                description = "Declare a series of attacks to try to gain control of additional countries.";break;
+        case 3: header = "Player "+std::to_string(playerNumber)+": Fortification Phase";
+                description = "Move a number of armies from one of your countries to one of your neighbor countries";break;
+        default: header = "Unknown";
+                description = "Unknown";
+    }
+}
---
+++ RISK_01/ObserverPlayerPhases.h
@@ -0,0 +1,16 @@
+#include <string>
+class ObserverPlayerPhases{
+    private:
+        std::string description; //description of phase
+        int phaseNumber; //number of the phase
+        int playerNumber;
+        std::string header; //includes player number and phase title
+        void setPhaseMessage();
+
+    public:
+        std::string getWholeMessage();//includes header, description
+        
+        void setHeaderDescription(int phaseNumber2, int playerNumber);//includes the phase number and the player number
+        
+
+};
